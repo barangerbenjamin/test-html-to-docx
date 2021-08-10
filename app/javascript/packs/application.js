775dcc2e48d24d375ddd32f7db945b2f20210396
Benjamin Baranger <baranger.benjamin@gmail.com>
2021-08-10 10:21:01 +0100

    add btn to trix toolbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,41 @@ import "controllers"
 
 require("trix")
 require("@rails/actiontext")
+
+
+var Trix = require("trix");
+
+Trix.config.blockAttributes.heading1 = {
+  tagName: 'h1',
+  terminal: true,
+  breakOnReturn: true,
+  group: false
+
+}
+Trix.config.blockAttributes.heading2 = {
+  tagName: 'h2',
+  terminal: true,
+  breakOnReturn: true,
+  group: false
+}
+Trix.config.blockAttributes.heading3 = {
+  tagName: 'h3',
+  terminal: true,
+  breakOnReturn: true,
+  group: false
+}
+
+/* insert the button visual in the default toolbar */
+addEventListener("trix-initialize", function (event) {
+  var h1 = `<button type="button" class="trix-button" data-trix-attribute="heading1" title="Heading1" tabindex="-1">H1</button>`
+  var h2 = `<button type="button" class="trix-button" data-trix-attribute="heading2" title="Heading2" tabindex="-1">H2</button>`
+  var h3 = `<button type="button" class="trix-button" data-trix-attribute="heading3" title="Heading3" tabindex="-1">H3</button>`
+
+  toolbar = event.target.toolbarElement.querySelector(".trix-button-group")
+  toolbar.insertAdjacentHTML("afterbegin", h3)
+  toolbar.insertAdjacentHTML("afterbegin", h2)
+  toolbar.insertAdjacentHTML("afterbegin", h1)
+
+  // remove old header btn
+  document.querySelectorAll(".trix-button-group")[1].querySelector('button').remove()
+})
